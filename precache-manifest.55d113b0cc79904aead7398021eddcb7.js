6050cd8f64247464318c879dc0f118376395f975
Kristian Martinez <kmartman94@gmail.com>
2020-10-01 16:29:01 -0700

    Updates

---
+++ precache-manifest.55d113b0cc79904aead7398021eddcb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc4b769ac7d8ed272007ec76878a5286",
+    "url": "/monsterRolodex/index.html"
+  },
+  {
+    "revision": "e28b74ba5fbf546f95ae",
+    "url": "/monsterRolodex/static/css/main.a0924885.chunk.css"
+  },
+  {
+    "revision": "575b699cf6adaadc3609",
+    "url": "/monsterRolodex/static/js/2.5d3e688f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsterRolodex/static/js/2.5d3e688f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e28b74ba5fbf546f95ae",
+    "url": "/monsterRolodex/static/js/main.02885547.chunk.js"
+  },
+  {
+    "revision": "47b46d6f15d610d10ab6",
+    "url": "/monsterRolodex/static/js/runtime-main.83640a90.js"
+  }
+]);
